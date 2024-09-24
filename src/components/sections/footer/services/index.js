5d0c0614e0d736dd9f666528f388d112f10d080e
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-09-24 23:39:23 +0330

    nav-top-1 and footer

--- src/components/sections/footer/services/index.js
+++ src/components/sections/footer/services/index.js
@@ -7,26 +7,27 @@ export default function Services() {
             services.map((section , index) => {
                 return <div
                     key={index}
-                    className={`${index ? "border-t-1" : ""} flex items-center`}
+                    className={`${index > 0 ? "border-t" : ""} h-20 flex items-center`}
                 >
                     {
                         section.map((service , index) => {
-                            return <a 
+                            return <div 
                                 key={index}
-                                href={service.href}
-                                className="flex-1 px-5 grid place-items-center"
+                                className="border-l last:border-none h-full flex-1 px-5 grid place-items-center"
                             >
-                                <Image 
-                                    nextHandle={true}
-                                    unoptimized={true}
-                                    fill={false}
-                                    height={20}
-                                    width={70}
-                                    imageClassName="w-auto h-5"
-                                    src={service.logo.src}
-                                    alt={service.logo.alt}
-                                />
-                            </a>
+                                <a href={service.href}>
+                                    <Image 
+                                        nextHandle={true}
+                                        unoptimized={true}
+                                        fill={false}
+                                        height={20}
+                                        width={70}
+                                        imageClassName="w-auto h-5"
+                                        src={service.logo.src}
+                                        alt={service.logo.alt}
+                                    />
+                                </a>
+                            </div>
                         })
                     }
                 </div>
