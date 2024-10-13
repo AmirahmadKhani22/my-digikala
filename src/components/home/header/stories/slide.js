c07674ba05bfada7ad91fe9801bc8479af2ef477
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-14 02:12:19 +0330

    add time badge to lives

--- src/components/home/header/stories/slide.js
+++ src/components/home/header/stories/slide.js
@@ -1,5 +1,5 @@
 // import {useState} from "react"
-import styles from "./home.module.css"
+import styles from "./styles.module.css"
 import Image from "@/components/utils/components/image"
 // import VideoModal from "./videoModal"
 
@@ -16,13 +16,19 @@ export default function Slides({item}) {
             // onClick={openModal}        
         >
         <div className="w-[84px]">
-            <div className={`w-full h-[84px] rounded-full p-0.5 ${styles["stories-image-border"]}`}>
+            <div className={`w-full h-[84px] relative rounded-full p-0.5 ${styles["stories-image-border"]}`}>
                 <Image
                     nextHandle={true}
                     fill={false}
-                    src={item.circle_cover}
+                    // src={item.circle_cover}
+                    // alt={item.title}
                     className={`rounded-[inherit] m-auto w-full h-full object-cover ${styles["stories-image"]}`}
                 />
+                {
+                    item["badge_text"] && <div className="w- h- px-1.5 absolute bottom-0 m-auto border bg-[#fdecf5] text">
+
+                    </div>
+                }
             </div>
             <p className={`mt-2 text-[11px] leading-6 text-center ${item.title.length > 25 ? "ellipsis-2" : ""}`}>
                 {item.title}
