fead171c70efc7a54c915e74bc4a9f02ae7b74c6
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-14 16:53:01 +0330

    recomendation products cleanup

--- src/components/home/main/recomendationProducts/wrapper.js
+++ src/components/home/main/recomendationProducts/wrapper.js
@@ -1,12 +1,10 @@
 "use client"
-import ProductLink from "@/components/sections/productLink"
 import AddSVGIcon from "@/components/utils/components/addSVGIcon"
 import Image from "@/components/utils/components/image"
 import Link from "@/components/utils/components/link"
 import handleURL from "@/components/utils/handleURL"
 
 export default function Wrapper({categories , part}) {
-    console.log(categories)
     return <div className="flex items-center justify-between border rounded-2xl">
         {
             categories.map((category , categoryIndex) => {
