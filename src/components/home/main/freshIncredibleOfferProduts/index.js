0cd3ee2917b1e1922e1f42764437961a2e785429
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-14 17:40:07 +0330

    fresh incredible offer cleanup

--- src/components/home/main/freshIncredibleOfferProduts/index.js
+++ src/components/home/main/freshIncredibleOfferProduts/index.js
@@ -7,15 +7,19 @@ import Products from "./products"
 
 export default function FreshIncredibleOfferProducts() {
     const {data} = useData("fresh_incredible_offer_products_circle")
-    const seeMore = handleURL(data["see_more_url"])
-    return <div className={`mt-4 relative bg-neutral-100 ${style["background-pattern"]} rounded-2xl`}>
-        <Link
-            href={seeMore}
-            className={`absolute inset-0 z-0 w-full h-full ${style["background-gradient"]} rounded-[inherit]`}
-        />
-        <div className="box-border min-h-28 w-full h-full flex items-center justify-between px-9 py-3.5 rounded-[inherit]">
-            <Info data={data}/>
-            <Products data={data} seeMore={seeMore} />
+    if(data && !!data.length) {
+        const seeMore = handleURL(data["see_more_url"])
+        return <div className={`mt-4 relative bg-neutral-100 ${style["background-pattern"]} rounded-2xl`}>
+            <Link
+                href={seeMore}
+                className={`absolute inset-0 z-0 w-full h-full ${style["background-gradient"]} rounded-[inherit]`}
+            />
+            <div className="box-border min-h-28 w-full h-full flex items-center justify-between px-9 py-3.5 rounded-[inherit]">
+                <Info data={data}/>
+                <Products data={data} seeMore={seeMore} />
+            </div>
         </div>
-    </div>
+    } else {
+        return null
+    }
 }
