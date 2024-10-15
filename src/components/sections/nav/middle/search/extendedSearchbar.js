e76c635a6c08d697856c038bc009ee46d0c33a87
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-15 17:30:06 +0330

    Navbar middle searchbar modal add seperate extended searchbar components to ExtndedSearchBar component

--- src/components/sections/nav/middle/search/extendedSearchbar.js
+++ src/components/sections/nav/middle/search/extendedSearchbar.js
@@ -1,3 +1,76 @@
-export default function ExtendedSearchBar() {
-    return <></>
+import {useEffect} from 'react'
+import Link from '@/components/utils/components/link'
+import SwiperType1Structure from '@/components/sections/swiper/type-1/structure'
+import swiperType1Config, {swiperType1Destroy} from '../../swiper/type-1/config'
+import AddSVGIcon from '@/components/utils/components/addSVGIcon'
+
+export default function ExtendedSearchBar({data , openModal}) {
+    useEffect(() => {
+        if(openModal) {
+            const swiper = swiperType1Config({
+                prefixSelector: "nav-related-searches", 
+                container: "#nav-related-searches-swiper-slides",
+                slidesPerView: "auto",
+            })
+            return () => {
+                swiperType1Destroy(swiper)
+            }
+        }
+    })
+    return <div 
+        className="absolute z-2 w-full h-auto border overflow-x-hidden overflow-y-auto bg-white py-5 rounded-[inherit] shadow-md"
+        onClick={event => event.stopPropagation()}
+    >
+        <div className="mx-4 [border-bottom:1px_solid_var(--color-secondary-500)]">
+            <input 
+                type="search"
+                placeholder="جستجو"
+                className="px-3 w-full outline-none pb-3 text-xs"
+            />
+        </div>
+        <div className="px-4">
+            <picture className="px-4"></picture>
+        </div>
+        {/* searches history */}
+        {}
+        <div className="w-full border-b">
+            <div className="px-4 flex items-center gap-x-3">
+                <AddSVGIcon
+                    href="/symbol/search-trend.svg"
+                    id="search-trend"
+                    className="size-7 fill icon-low-emphasis"
+                />
+                <p>جستجو&zwnj;های پرطرفدار</p>
+            </div>
+            {
+                data && <div className="my-4 w-full">
+                    <SwiperType1Structure
+                        id="nav-related-searches-swiper"
+                        prefixSelector="nav-related-searches"
+                        containerClassName="w-full h-auto pr-4 py-1"
+                    >
+                        {
+                            data.map((info , index) => (
+                                <div key={index} className="swiper-slide w-auto">
+                                    <div className="flex gap-x-2 items-center w-fit pl-1 ml-2 py-2 pr-3 border rounded-full">
+                                        <Link 
+                                            href={info.href}
+                                            className="cursor-text text-sm"
+                                        >
+                                            {info.text}
+                                        </Link>
+                                        <AddSVGIcon
+                                            href="/symbol/left-1.svg"
+                                            id="left-1"
+                                            className="size-6"
+                                        />
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </SwiperType1Structure>
+                </div>
+            }
+        </div>
+    </div>
 }
--- src/components/sections/nav/middle/search/index.js
+++ src/components/sections/nav/middle/search/index.js
@@ -1,7 +1,4 @@
-import {useState , useEffect} from 'react'
-import Link from '@/components/utils/components/link'
-import SwiperType1Structure from '@/components/sections/swiper/type-1/structure'
-import swiperType1Config, {swiperType1Destroy} from '../../swiper/type-1/config'
+import {useState} from 'react'
 import uniqueKey from '@/components/utils/generateRandomElementKey'
 import AddSVGIcon from '@/components/utils/components/addSVGIcon'
 import ModalBackground from '@/components/utils/components/modalBackground'
@@ -10,18 +7,6 @@ import ExtendedSearchBar from './extendedSearchbar'
 export default function Search({relatedSearchSlidesInfo}) {
     const [openModal , setOpenModal] = useState(false)
     // get searches history from local-storage
-    useEffect(() => {
-        if(openModal) {
-            const swiper = swiperType1Config({
-                prefixSelector: "nav-related-searches", 
-                container: "#nav-related-searches-swiper-slides",
-                slidesPerView: "auto",
-            })
-            return () => {
-                swiperType1Destroy(swiper)
-            }
-        }
-    })
     const handleOpenModal = () => setOpenModal(true)
     const handleCloseModal = () => setOpenModal(false)
     return <div 
@@ -30,63 +15,10 @@ export default function Search({relatedSearchSlidesInfo}) {
     >
         {openModal ? 
             <>
-                <ExtendedSearchBar />
-                <div 
-                    className="absolute z-2 w-full h-auto border overflow-x-hidden overflow-y-auto bg-white py-5 rounded-[inherit] shadow-md"
-                    onClick={event => event.stopPropagation()}
-                >
-                    <div className="mx-4 [border-bottom:1px_solid_var(--color-secondary-500)]">
-                        <input 
-                            type="search"
-                            placeholder="جستجو"
-                            className="px-3 w-full outline-none pb-3 text-xs"
-                        />
-                    </div>
-                    <div className="px-4">
-                        <picture className="px-4"></picture>
-                    </div>
-                    {/* searches history */}
-                    {}
-                    <div className="w-full border-b">
-                        <div className="px-4 flex items-center gap-x-3">
-                            <AddSVGIcon
-                                href="/symbol/search-trend.svg"
-                                id="search-trend"
-                                className="size-7 fill icon-low-emphasis"
-                            />
-                            <p>جستجو&zwnj;های پرطرفدار</p>
-                        </div>
-                        {
-                            relatedSearchSlidesInfo && <div className="my-4 w-full">
-                                <SwiperType1Structure
-                                    id="nav-related-searches-swiper"
-                                    prefixSelector="nav-related-searches"
-                                    containerClassName="w-full h-auto pr-4 py-1"
-                                >
-                                    {
-                                        relatedSearchSlidesInfo.map((info , index) => (
-                                            <div key={index} className="swiper-slide w-auto">
-                                                <div className="flex gap-x-2 items-center w-fit pl-1 ml-2 py-2 pr-3 border rounded-full">
-                                                    <Link 
-                                                        href={info.href}
-                                                        className="cursor-text text-sm"
-                                                    >
-                                                        {info.text}
-                                                    </Link>
-                                                    <AddSVGIcon
-                                                        href="/symbol/left-1.svg"
-                                                        id="left-1"
-                                                        className="size-6"
-                                                    />
-                                                </div>
-                                            </div>
-                                        ))
-                                    }
-                                </SwiperType1Structure>
-                            </div>
-                        }
-                    </div>
-                </div>               
+                <ExtendedSearchBar
+                    data={relatedSearchSlidesInfo}
+                    openModal={openModal}
+                />
                 {
                     createPortal(
                         <ModalBackground
