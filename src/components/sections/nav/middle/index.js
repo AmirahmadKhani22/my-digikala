2a197cc27f9dab845257b433cd0578baf693fdf7
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-15 16:16:03 +0330

    Navbar middle login button modification flex gap-x

--- src/components/sections/nav/middle/index.js
+++ src/components/sections/nav/middle/index.js
@@ -20,7 +20,7 @@ export default function Middle() {
             <Search relatedSearchSlidesInfo={relatedSearchSlidesInfo}/>
         </div>
         <div className="flex-1 flex items-center justify-end">
-            <div className="flex items-center pt-2 pb-[10px] px-4 border rounded-lg">
+            <div className="flex items-center gap-x-2 pt-2 pb-[10px] px-4 border rounded-lg">
                 <AddSVGIcon
                     href="/symbol/login-1.svg"
                     id="login-1"
