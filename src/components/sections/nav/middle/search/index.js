e6947efbe264ac318ef179908c6d4d2dfc40c796
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-15 17:24:46 +0330

    Navbar middle searchbar modal add magnify-glass icon

--- src/components/sections/nav/middle/search/index.js
+++ src/components/sections/nav/middle/search/index.js
@@ -100,11 +100,6 @@ export default function Search({relatedSearchSlidesInfo}) {
                 }
             </> :
             <div className="w-full h-11 py-3 px-5 bg-neutral-100 flex items-center gap-x-5 rounded-[inherit]">
-                <FaMagnifyingGlass 
-                    size="24px" 
-                    color="var(--color-icon-low-empahsis)"
-                    className=""
-                />
                 <AddSVGIcon
                     href="/symbol/magnify-glass-1.svg"
                     id="search-1"
