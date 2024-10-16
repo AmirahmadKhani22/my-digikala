34e7c840f70ccb1d61475632b5a7bdf7f438e5c9
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-16 19:14:20 +0330

    Navbar details categories add title

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 // 320-360-640-768-1024-1280-1336-1440-1676 breakpoints min-width
-import "./src/components/sections/nav/"
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
