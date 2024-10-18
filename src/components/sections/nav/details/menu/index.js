1f37f320b0e1e83df0865f303482a7f1aeeb510b
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-18 21:45:43 +0330

    Navbar details menu add other items

--- src/components/sections/nav/details/menu/index.js
+++ src/components/sections/nav/details/menu/index.js
@@ -3,25 +3,43 @@ import Link from "@/components/utils/components/link"
 import AddSVGIcon from "@/components/utils/components/addSVGIcon"
 
 export default function Menu() {
-    return <menu className="px-3 border-l border-r flex items-center gap-x-6">
-        {
-            data.map((item , index) => {
-                return <li 
-                    key={index}
-                >
-                    <Link
-                        href={item.link}
-                        className="w-full h-full flex items-center gap-x-2"
+    return <div>
+        <menu className="px-3 border-l border-r flex items-center gap-x-6">
+            {
+                data.map((item , index) => {
+                    return <li 
+                        key={index}
                     >
-                        <AddSVGIcon
-                            href={item.icon.href}
-                            id={item.icon.id}
-                            className="size-4 fill-icon-low-emphasis"
-                        />
-                        <span className="text-[13px] leading-4 text-neutral-600">{item.title}</span>
-                    </Link>
-                </li>
-            })
-        }
-    </menu>
+                        <Link
+                            href={item.link}
+                            className="w-full h-full flex items-center gap-x-2"
+                        >
+                            <AddSVGIcon
+                                href={item.icon.href}
+                                id={item.icon.id}
+                                className="size-4 fill-icon-low-emphasis"
+                            />
+                            <span className="text-[13px] leading-4 text-neutral-600">{item.title}</span>
+                        </Link>
+                    </li>
+                })
+            }
+        </menu>
+        <ul>
+            <li>
+                <Link
+                    href="/faq/"
+                >
+                    سوالی دارید؟
+                </Link>
+            </li>
+            <li>
+                <Link
+                    href="/landings/seller-introduction/"
+                >
+                    در دیجی‌کالا بفروشید!
+                </Link>
+            </li>
+        </ul>
+    </div>
 }
