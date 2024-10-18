b4910048786dbb73501701ff7f18bd1168a50592
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-18 14:25:57 +0330

    Navbar details menu create component structure

--- src/components/sections/nav/details/menu/index.js
+++ src/components/sections/nav/details/menu/index.js
@@ -1,7 +1,25 @@
 import data from "./data.json"
+import Link from "@/components/utils/components/link"
+import AddSVGIcon from "@/components/utils/components/addSVGIcon"
 
 export default function Menu() {
     return <menu>
-        
+        {
+            data.map((item , index) => {
+                return <li 
+                    key={index}
+                >
+                    <Link
+                        href={item.link}
+                    >
+                        <AddSVGIcon
+                            href={item.icon.href}
+                            id={item.icon.id}
+                        />
+                        <span>{item.title}</span>
+                    </Link>
+                </li>
+            })
+        }
     </menu>
 }
