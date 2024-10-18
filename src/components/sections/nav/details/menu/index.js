8ab31aaf1304ea31c34a362a7b80b56755ab6521
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-18 21:59:49 +0330

    Navbar details menu styling other items

--- src/components/sections/nav/details/menu/index.js
+++ src/components/sections/nav/details/menu/index.js
@@ -3,7 +3,7 @@ import Link from "@/components/utils/components/link"
 import AddSVGIcon from "@/components/utils/components/addSVGIcon"
 
 export default function Menu() {
-    return <div>
+    return <div className="flex items-center gap-x-3">
         <menu className="px-3 border-l border-r flex items-center gap-x-6">
             {
                 data.map((item , index) => {
@@ -19,25 +19,27 @@ export default function Menu() {
                                 id={item.icon.id}
                                 className="size-4 fill-icon-low-emphasis"
                             />
-                            <span className="text-[13px] leading-4 text-neutral-600">{item.title}</span>
+                            <span className="text-[12px] leading-4 text-neutral-600">{item.title}</span>
                         </Link>
                     </li>
                 })
             }
         </menu>
-        <ul>
+        <ul className="flex items-center gap-x-6">
             <li>
                 <Link
                     href="/faq/"
+                    className="w-full h-full flex items-center"
                 >
-                    سوالی دارید؟
+                    <span className="text-[12px] leading-4 text-neutral-600">سوالی دارید؟</span>
                 </Link>
             </li>
             <li>
                 <Link
                     href="/landings/seller-introduction/"
+                    className="w-full h-full flex items-center"
                 >
-                    در دیجی‌کالا بفروشید!
+                    <span className="text-[12px] leading-4 text-neutral-600">در دیجی‌کالا بفروشید!</span>
                 </Link>
             </li>
         </ul>
