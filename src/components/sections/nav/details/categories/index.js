f2dce969824a9fe9cb2b9aef2da703c4119da19e
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-24 02:28:23 +0330

    Navbar categories button handle hover event

--- src/components/sections/nav/details/categories/index.js
+++ src/components/sections/nav/details/categories/index.js
@@ -1,9 +1,21 @@
+import {useState} from "react"
 import styles from "../hover.module.css"
 import AddSVGIcon from "@/components/utils/components/addSVGIcon"
 
 export default function Categories() {
+    const [showDetails , setShowDetails] = useState(false)
+    const handleOnMouseEnter = event => {
+        setShowDetails(true)
+    }
+    const handleOnMouseLeave = event => {
+        setShowDetails(false)
+    }
     return <div className="relative">
-        <button className={`flex items-center gap-x-2 ${styles["on-hover-underline"]}`}>
+        <button 
+            className={`flex items-center gap-x-2 ${styles["on-hover-underline"]}`}
+            onMouseEnter={handleOnMouseEnter}
+            onMouseLeave={handleOnMouseLeave}
+        >
             <AddSVGIcon 
                 href="/symbol/hamburger-menu.svg"
                 id="hamburger-menu"
@@ -11,8 +23,8 @@ export default function Categories() {
             />
             <span className="text-sm leading-4 font-bold text-icon-high-emphasis">دسته&zwnj;بندی کالاها</span>
         </button>
-        <div className="absolute">
-
-        </div>
+        {
+            showDetails && <Modal />
+        }
     </div>
 }
