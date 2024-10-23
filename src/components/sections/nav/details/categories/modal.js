95c790ebf42973e9d21a1d39713b482a015d310b
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-24 03:06:04 +0330

    Navbar categories modal categories container styling

--- src/components/sections/nav/details/categories/modal.js
+++ src/components/sections/nav/details/categories/modal.js
@@ -4,14 +4,14 @@ import uniqueKey from "@/components/utils/generateRandomElementKey"
 
 export default function Modal() {
     return <>
-        <div className="absolute w-auto h-[85vh]">
+        <div className="absolute right-0 top-full z-[-1] w-auto h-[85vh] bg-white rounded-lg shadow-lg">
 
         </div>
         {
             createPortal(
                 <BackgroundModal 
                     containerClassName="z-[999]"
-                    backgrondClassName="bg-[rgba(0,0,0,0.5)]"
+                    backgrondClassName="bg-[rgba(0,0,0,0.35)]"
                 />,
                 document.body,
                 uniqueKey()
