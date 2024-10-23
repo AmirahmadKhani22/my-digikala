082b38a4901ca8d510afda696504db647871ec4e
AmirahmadKhani22 <130404068+AmirahmadKhani22@users.noreply.github.com>
2024-10-24 03:25:35 +0330

    Navbar categories modal categories container styling

--- src/components/sections/nav/details/categories/modal.js
+++ src/components/sections/nav/details/categories/modal.js
@@ -4,8 +4,8 @@ import uniqueKey from "@/components/utils/generateRandomElementKey"
 
 export default function Modal() {
     return <>
-        <div className="absolute right-0 top-[calc(100%+15px-2px)] z-[-1] w-auto h-[85vh] bg-white rounded-b-lg border shadow-sm">
-            
+        <div className="flex absolute right-0 top-[calc(100%+15px-2px)] z-[-1] w-auto h-[85vh] bg-white rounded-b-lg border shadow-sm">
+
         </div>
         {
             createPortal(
